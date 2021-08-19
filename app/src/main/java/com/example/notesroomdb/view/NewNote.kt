7573f2a56f8f046a221ec8e9196571ff76c2b9ab
csa67 <saiaarathy@gmail.com>
2021-08-19 16:53:16 +0530

    UI updates for new note activity

--- app/src/main/java/com/example/notesroomdb/view/NewNote.kt
+++ app/src/main/java/com/example/notesroomdb/view/NewNote.kt
@@ -22,6 +22,7 @@ class NewNote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_new_note)
+        title = "New Note"
         setupViewModel()
         setupView()
     }
